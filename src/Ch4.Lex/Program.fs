6f1c81a4f60a8b3e6335533d1a04abf5262dba88
guricerin <chanbo1e9@gmail.com>
2021-11-09 21:53:52 +0900

    ch4 done

--- src/Ch4.Lex/Program.fs
+++ src/Ch4.Lex/Program.fs
@@ -1,12 +1,27 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.IO
+open FSharp.Text.Lexing
+open Ch4.Lex
 
-// Define a function to construct a message to print
-let from whom = sprintf "from %s" whom
+let top file =
+    let stream =
+        File.ReadAllLines(file)
+        |> fun arr -> String.Join(" ", arr)
+        |> String.Concat
+        |> fun x -> x.Split([| " " |], StringSplitOptions.RemoveEmptyEntries)
+
+    for s in stream do
+        s
+        |> LexBuffer<_>.FromString
+        |> Lexer.main
+        |> fun tok -> tok.ToString()
+        |> printfn "%s"
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    if argv.Length = 1 then
+        top argv.[0]
+    else
+        ()
+
     0 // return an integer exit code
---
+++ src/Ch4.Lex/Token.fs
@@ -0,0 +1,18 @@
+module Ch4.Lex.Token
+
+[<RequireQualifiedAccess>]
+type Token =
+    | EOF
+    | UNDERBAR
+    | ID of string
+    | STRING of string
+    | REAL of string
+    | SPECIAL of string
+    override this.ToString() =
+        match this with
+        | EOF -> "EOF "
+        | ID s -> $"ID {s}"
+        | REAL s -> $"REAL {s}"
+        | STRING s -> $"STRING \"{s}\""
+        | UNDERBAR -> "UNDERBAR "
+        | SPECIAL s -> $"SPECIAL {s}"
